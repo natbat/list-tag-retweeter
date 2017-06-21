81ca2c34a1568bdf164b819c15a572d57b2a7983
Natalie Downe <natalie@eventbrite.com>
2017-06-20 20:26:43 -0700

    biotweeps config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import datetime
 
-START_DATE = datetime.date(2017, 6, 11)
-END_DATE = datetime.date(2017, 6, 13)
-HASHTAG = '#notavampirebat'
-LIST_OWNER_SCREEN_NAME = 'flightynat'
-LIST_SLUG = 'speakers'
+START_DATE = datetime.date(2017, 6, 24)
+END_DATE = datetime.date(2017, 7, 2)
+HASHTAG = '#btcon17'
+LIST_OWNER_SCREEN_NAME = 'biotweeps'
+LIST_SLUG = 'btcon17'
