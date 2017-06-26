2fc84db829c5bafa9f79aea76941a339a7df4866
Natalie Downe <natalie@eventbrite.com>
2017-06-26 09:01:35 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import datetime
 
-START_DATE = datetime.date(2017, 6, 24)
+START_DATE = datetime.date(2017, 6, 27)
 END_DATE = datetime.date(2017, 7, 2)
 HASHTAG = '#btcon17'
 LIST_OWNER_SCREEN_NAME = 'biotweeps'
